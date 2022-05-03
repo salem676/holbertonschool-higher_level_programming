38fd1015a328736dfbf3a2291ecf164dbccb7b92
salem676 <92064246+salem676@users.noreply.github.com>
2022-05-02 21:25:11 -0500

    Delete 6-print_comb3.py

--- 0x03-python-data_structures/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(0, 10):
-        if (x < y):
-            print("{:d}{:d}".format(x, y), end="")
-            if x < 8:
-                print(", ", end="")
-print("")
