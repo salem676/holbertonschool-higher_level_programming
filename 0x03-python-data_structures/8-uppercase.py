0182d66cdc256c5d3375d1f80688d68877914e57
salem676 <92064246+salem676@users.noreply.github.com>
2022-05-02 21:25:23 -0500

    Delete 8-uppercase.py

--- 0x03-python-data_structures/8-uppercase.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for character in str + "\n":
-        if (ord(character) >= 97 and ord(character) < 123):
-            print("{}".format(chr(ord(character) - 32)), end="")
-        else:
-            print("{}".format(character), end="")
