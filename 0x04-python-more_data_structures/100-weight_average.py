54f4f576aeeaabfcf7a506360397629c71d1854a
salem676 <echoesofeternal@gmail.com>
2022-05-08 11:37:51 -0500

    third

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,45 +1,16 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+def get_ponderated_promedium(scores):
+    list_promediums = list(map(lambda a: a[0] * a[1], scores))
+    return sum(list_promediums)
 
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+
+def get_sum_weight(scores):
+    list_weights = list(map(lambda a: a[1], scores))
+    return sum(list_weights)
+
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        average = get_p
