cbb8baeb4e3561fd9dad4f42266a613da183689e
salem676 <echoesofeternal@gmail.com>
2022-07-07 14:40:30 -0500

    california

--- 0x0E-SQL_more_queries/8-cities_of_california.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california.sql
@@ -1,10 +1,9 @@
--- lists all the cities of California in the database hbtn_0d_usa
-   -- states table contains only one record where name = California
-   -- Results must be sorted in ascending order by cities.id
-   -- The database name will be passed as an argument of the mysql command
-
-SELECT id, name FROM cities
-WHERE state_id = (
-      SELECT id FROM states
-      WHERE name = "California")
-ORDER BY id;
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
