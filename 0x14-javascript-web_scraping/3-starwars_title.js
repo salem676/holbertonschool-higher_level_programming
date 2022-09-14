1c600ac444accc2998b3f961ee800bbbe5c92c16
salem676 <echoesofeternal@gmail.com>
2022-09-14 14:19:27 -0500

    oupo

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const let url = 'http://swapi.co/api/films/';
-const let episode = process.argv[2];
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
 });
