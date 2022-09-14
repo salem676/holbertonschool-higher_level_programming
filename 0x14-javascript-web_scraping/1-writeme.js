4b5be5ed978e60a03edb78d62f24e8d0fe6dbb15
salem676 <echoesofeternal@gmail.com>
2022-09-14 13:07:30 -0500

    make it real

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const file = process.argv[2];
-const content = process.argv[3];
-fs.writeFile(file, content, 'utf-8', function (err) {
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
